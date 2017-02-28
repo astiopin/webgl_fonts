b96ad4fbb6de993e9e5426f0066ff576c91102da
Anton Stepin <aastepin@yandex.ru>
2017-02-28 14:00:15 +0300

    Updated subpixel coverage calculation

--- src/fshader.js
+++ src/fshader.js
@@ -20,7 +20,7 @@ var fragCode=`
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- */  
+ */                  
 
 precision mediump float;
 
@@ -36,62 +36,75 @@ varying float doffset;
 varying float sdf_texel;
 
 
-// Wrote this routine a couple years ago without a single line
-// of a commentary. Seemed obvious back than. Now it looks
-// like some black magic. Write comments.
+/*
+ *  Subpixel coverage calculation
+ *    
+ *  v - edge slope                         triplet
+ *  a - pixel coverage                     coverage
+ *                                       
+ *      |<- glyph edge                      R  G  B
+ *  +---+---+                             +--+--+--+
+ *  |   |XXX| v = 1.0 (edge facing west)  |  |xx|XX|
+ *  |   |XXX| a = 0.5 (50% coverage)      |  |xx|XX|
+ *  |   |XXX|                             |  |xx|XX|
+ *  +---+---+                             +--+--+--+
+ *    pixel                                0  50 100
+ *
+ *
+ *        R   G   B
+ *         
+ *   1.0        +--+   <- top (abs( v ))
+ *              |  
+ *       -+-----+--+-- <- ceil: 100% coverage
+ *        |     |XX|     
+ *   0.0  |  +--+XX|     
+ *        |  |xx|XX|     
+ *       -+--+--+--+-- <- floor: 0% coverage
+ *           |
+ *  -1.0  +--+         <-  -abs(v)
+ *        |
+ *        |
+ *        |         
+ *  -2.0  +            <- bottom: -abs(v)-1.0
+ */
 
 vec3 subpixel( float v, float a ) {
-    float av = abs( v );
-    float pv = clamp(  v, 0.0, 1.0 );
-    float nv = clamp( -v, 0.0, 1.0 );
-
-    vec3 rgb_max = mix( 
-        vec3( 1.0,    1.0,    1.0   ), 
-        vec3( 0.333,  0.833,  0.333 ), 
-        vec3( nv,     av,     pv    ) );
-    
-    vec3 rgb_min = mix( 
-        vec3( 0.0,    0.0,    0.0   ), 
-        vec3( 0.667,  0.167,  0.667 ), 
-        vec3( pv,     av,     nv    ) );
-    
-    vec3 rgb_range  = rgb_max - rgb_min;
-    vec3 rgb_cut    = clamp( vec3( a ), rgb_min, rgb_max );
-    vec3 res        = ( rgb_cut - rgb_min ) / rgb_range;
-    res = clamp( res, 0.0, 1.0 );
+    float vt      = 0.35 * v; // 1.0 will make your eyes bleed
+    vec3  rgb_max = vec3( -vt, 0.0, vt );
+    float top     = abs( vt );
+    float bottom  = -top - 1.0;
+    float cfloor  = mix( top, bottom, a );
+    vec3  res     = clamp( rgb_max - vec3( cfloor ), 0.0, 1.0 );
     return res;
 }
 
 
 void main() {
-    // Sampling the texture, L pattern
+    // Sampling the texture
     float sdf       = texture2D( font_tex, tc0 ).r;
     float sdf_north = texture2D( font_tex, tc0 + vec2( 0.0, sdf_texel ) ).r;
     float sdf_east  = texture2D( font_tex, tc0 + vec2( sdf_texel, 0.0 ) ).r;
 
-    // Estimating stroke direction by the distance field gradient vector
-    vec2  sgrad     = vec2( sdf_east - sdf, sdf_north - sdf );
-    float sgrad_len = max( length( sgrad ), 1.0 / 128.0 );
-    vec2  grad      = sgrad / vec2( sgrad_len );
+    // Estimating stroke direction by the distance field gradient
+    vec2  grad  = normalize( vec2( sdf_east - sdf, sdf_north - sdf ) + vec2( 1e-5 ) /* Beware the NaNs */ );
     float vgrad = abs( grad.y ); // 0.0 - vertical stroke, 1.0 - horizontal one
     
-    float horz_scale  = 1.1; // Blurring vertical strokes along the X axis a bit
-    float vert_scale  = 0.6; // While adding some contrast to the horizontal strokes
-    float hdoffset    = mix( doffset * horz_scale, doffset * vert_scale, vgrad ); 
+    float horz_scale = 1.1; // Blurring vertical strokes along X a bit
+    float vert_scale = 0.7; // While adding some contrast to the horizontal strokes
+    float hdoffset   = mix( doffset * horz_scale, doffset * vert_scale, vgrad );    
     float res_doffset = mix( doffset, hdoffset, hint_amount );
     
     float alpha       = smoothstep( 0.5 - res_doffset, 0.5 + res_doffset, sdf );
+    // Additional contrast
+    alpha             = pow( alpha, 1.0 + 0.2 * vgrad * hint_amount );
 
-    // Unfortunately there is no support for ARB_blend_func_extended in WebGL.
-    // Fortunately the background is filled with a solid color so we can do
-    // the blending inside the shader.
     
-    // Discarding pixels beyond a threshold to minimise possible artifacts.
+    // Unfortunately there is no support for ARB_blend_func_extended in WebGL
+    // So we're blending glyph pixels with the background inside the shader
+    // discarding pixels beyond the alpha threshold
     if ( alpha < 20.0 / 256.0 ) discard;
     
-    vec3 channels = subpixel( grad.x * 0.5 * subpixel_amount, alpha );
-
-    // For subpixel rendering we have to blend each color channel separately
+    vec3 channels = subpixel( grad.x * subpixel_amount, alpha );
     vec3 res = mix( bg_color, font_color, channels );
     
     gl_FragColor = vec4( res, 1.0 );
